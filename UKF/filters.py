6469a5911125ce2f21f2a3be82e327f36ab0eaf9
AhmedMAyman <ahmedmayman2222@gmail.com>
2017-04-21 08:41:19 +0200

    minimizing variables names
    also adding glossary

--- UKF/filters.py
+++ UKF/filters.py
@@ -5,80 +5,93 @@ import cov, models
 
 
 
-def ukf(current_state_mean, current_state_error_cov, measurement, Ts, process_noise, measurement_noise):
+def ukf(current_state_mean,current_state_error_cov,measurement,Ts,process_noise,measurement_noise):
+    
+    # Glossary:
+    #
+    # nx: state vector size
+    # ssp: state sigma points
+    # msp: measurement sigma points
+    # pnssp: predicted next state sigma points
+    # ussp: update state sigma points
+    # umsp: updated measurement sigma points
+    # zn: zero noise
+    # st: state telda
+    # pnsm: predicted next state mean
+    # pnsec: predicted next state error covariance
+    # mm: measurement mean
+    # mc: measurement covariance
+    # ust: updated state telda
+    # pmm: predicted measurement mean
+    # pmc: predicted measurement covariance
+    # pmcc: predicted measurement cross covariance
+    # kg: kalman gain
 
-    current_state_mean = np.pad(current_state_mean,(0,len(measurement)-len(current_state_mean)),'constant') #pad the current state mean vector with 0s for the new beacons data
+    # pad the current state mean vector with 0s for the new beacons data
+    current_state_mean = np.pad(current_state_mean,(0,len(measurement)-len(current_state_mean)),'constant')
 
-    state_vector_size = len(current_state_mean)
+    nx = len(current_state_mean)
 
-    state_sigma_points = np.zeros(shape=(state_vector_size, 2 * state_vector_size))
-    measurement_sigma_points = np.zeros(shape=(state_vector_size, 2 * state_vector_size))
-    predicted_next_state_sigma_points = np.zeros(shape=(state_vector_size, 2 * state_vector_size))
-    updated_state_sigma_points = np.zeros(shape=(state_vector_size, 2 * state_vector_size))
-    updated_measurement_sigma_points = np.zeros(shape=(state_vector_size, 2 * state_vector_size))
+    ssp = np.zeros(shape=(nx, 2 * nx))
+    msp = np.zeros(shape=(nx, 2 * nx))
+    pnssp = np.zeros(shape=(nx, 2 * nx))
+    ussp = np.zeros(shape=(nx, 2 * nx))
+    umsp = np.zeros(shape=(nx, 2 * nx))
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%
     # % 1) get the segma points %
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%
-    state_telda = np.linalg.cholesky(state_vector_size * current_state_error_cov)
+    st = np.linalg.cholesky(nx * current_state_error_cov)
 
-    for i in range(0, state_vector_size):
-        state_sigma_points.T[i] = current_state_mean + state_telda.T[i]
+    for i in range(0, nx):
+        ssp.T[i] = current_state_mean + st.T[i]
 
-    for i in range(state_vector_size, 2*state_vector_size):
-        state_sigma_points.T[i] = current_state_mean - state_telda.T[i - state_vector_size]
+    for i in range(nx, 2*nx):
+        ssp.T[i] = current_state_mean - st.T[i - nx]
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     # % 2) time update (prediction step) %
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for i in range(0, 2*state_vector_size):
-        predicted_next_state_sigma_points.T[i] = models.process_model(state_sigma_points.T[i], Ts, process_noise)
+    for i in range(0, 2*nx):
+        pnssp.T[i] = models.process_model(ssp.T[i], Ts, process_noise)
 
-    predicted_next_state_mean = np.mean(predicted_next_state_sigma_points, 1)
-    predicted_next_state_error_cov = cov.covariance_vector(predicted_next_state_sigma_points, predicted_next_state_mean,
-                                                       predicted_next_state_sigma_points, predicted_next_state_mean,
-                                                       2 * state_vector_size)
+    pnsm = np.mean(pnssp, 1)
+    pnsec = cov.covariance_vector(pnssp, pnsm, pnssp, pnsm, 2 * nx)
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     # % 3) estimate measurement mean and covariance using sigma points %
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for i in range(0, 2 * state_vector_size):
-        measurement_sigma_points.T[i] = models.measurement_model(state_sigma_points.T[i], Ts, measurement_noise)
+    for i in range(0, 2 * nx):
+        msp.T[i] = models.measurement_model(ssp.T[i], Ts, measurement_noise)
 
-    measurement_mean = np.mean(measurement_sigma_points, 1)
-    measurement_cov = cov.covariance_vector(measurement_sigma_points, measurement_mean, measurement_sigma_points,
-                                        measurement_mean, 2 * state_vector_size)
+    mm = np.mean(msp, 1)
+    mc = cov.covariance_vector(msp, mm, msp, mm, 2 * nx)
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%
     # % 4) sigma points update %
     # %%%%%%%%%%%%%%%%%%%%%%%%%%
-    updated_state_telda = np.linalg.cholesky(state_vector_size * predicted_next_state_error_cov)
+    ust = np.linalg.cholesky(nx * pnsec)
 
-    for i in range(0, state_vector_size):
-        updated_state_sigma_points.T[i] = predicted_next_state_mean + updated_state_telda.T[i]
+    for i in range(0, nx):
+        ussp.T[i] = pnsm + ust.T[i]
 
-    for i in range(state_vector_size, 2 * state_vector_size):
-        updated_state_sigma_points.T[i] = predicted_next_state_mean - updated_state_telda.T[i - state_vector_size]
+    for i in range(nx, 2 * nx):
+        ussp.T[i] = pnsm - ust.T[i - nx]
 
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     # % 5) measurement update (filter step) %
     # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for i in range(0, 2 * state_vector_size):
-        updated_measurement_sigma_points.T[i] = models.measurement_model(updated_state_sigma_points.T[i], Ts, measurement_noise)
+    for i in range(0, 2 * nx):
+        umsp.T[i] = models.measurement_model(ussp.T[i], Ts, measurement_noise)
 
-    predicted_measurement_mean = np.mean(updated_measurement_sigma_points, 1)
-    predicted_measurement_cov = cov.covariance_vector(updated_measurement_sigma_points, predicted_measurement_mean,
-                                                  updated_measurement_sigma_points, predicted_measurement_mean,
-                                                  2 * state_vector_size)
+    pmm = np.mean(umsp, 1)
+    pmc = cov.covariance_vector(umsp, pmm, umsp, pmm, 2 * nx) + mc
 
-    predicted_state_measurement_cross_cov = cov.covariance_vector(updated_state_sigma_points, predicted_next_state_mean,
-                                                              updated_measurement_sigma_points,
-                                                              predicted_measurement_mean, 2 * state_vector_size)
+    psmcc = cov.covariance_vector(ussp, pnsm, umsp, pmm, 2 * nx)
 
-    kalman_gain = np.dot(predicted_state_measurement_cross_cov, np.linalg.inv(predicted_measurement_cov))
+    kg = np.dot(psmcc, np.linalg.inv(pmc))
 
-    filtered_state_mean = predicted_next_state_mean + np.dot(kalman_gain, (measurement - predicted_measurement_mean))
-    filtered_state_cov = predicted_next_state_error_cov - np.dot(np.dot(kalman_gain, predicted_measurement_cov),
-                                                                 kalman_gain.T)
+    filtered_state_mean = pnsm + np.dot(kg, (measurement - pmm))
+    filtered_state_cov = pnsec - np.dot(np.dot(kg, pmc), kg.T)
 
     return [filtered_state_mean, filtered_state_cov]
